d57972989f10b54549f5816adbed13e606e92e1d
Michael David Hall <60527849+Soulstealer07@users.noreply.github.com>
2020-04-15 13:26:47 -0500

    Update TexasCounty.R

--- TexasCounty.R
+++ TexasCounty.R
@@ -1,3 +1,14 @@
+library(ggmap)
+library(ggplot2)
+library(dplyr)
+library(rjson)
+library(jsonlite)
+library(RCurl)
+library(googleVis)
+library(rvest)
+
+
+
 TexasCounty<-'https://en.wikipedia.org/wiki/List_of_counties_in_Texas'
 TexasCounty_pop<-read_html(TexasCounty)
 TexasCounty_pop <- TexasCounty_pop %>%
@@ -8,19 +19,13 @@ TexasCounty_pop <- TexasCounty_pop[-c(3,4,5,6,9)]
 head(TexasCounty_pop)
 str(TexasCounty_pop)
 #This table has relevant population and size data
- 
+
 library(maps)
 counties<-map_data("county")
 Texas <- map('county','texas',fill=TRUE, col=palette())
 
 
-library(ggmap)
-library(ggplot2)
-library(dplyr)
-library(rjson)
-library(jsonlite)
-library(RCurl)
-library(googleVis)
+
 
 #This API has relevant location data latitude and longitude for each county in Texas
 base_url <- "https://data.texas.gov/resource/ups3-9e8m.json"
@@ -34,9 +39,10 @@ str(County_combine)
 #Combine Wikipedia Table and Json Data
 
 
-County_csv <- write.csv(County_combine,"C:/Users/micha/OneDrive/Documents/Test/TexasCounties/County_csv.csv")
+County_csv <- write.csv(County_combine,"C:/Users/micha/OneDrive/Documents/Test/TexasCounties/County.csv")
 
 
-
+TexasCountyMerger_10$LatLong <- paste(TexasCountyMerger_10$y_long,TexasCountyMerger_10$x_lat,sep=":")
 G6 <- gvisGeoChart(TexasCountyMerger_10,locationvar = "LatLong",colorvar='Population',sizevar = "AreaSQMILES",hovervar = "County",options=list(region="US-TX",displayMode="Markers",resolution="provinces",width=600,height=400))
 plot(G6)
+str(TexasCountyMerger_10)
